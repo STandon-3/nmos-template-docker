08828e49ea59b8fe1cb55f35cdc03477c05b5df3
Sonal Tandon <sonal.tandon@bbc.co.uk>
2021-09-27 18:05:04 +0100

    Removing the local install-dependencies

--- .render/.init-scripts/make-build-tools.sh
+++ .render/.init-scripts/make-build-tools.sh
@@ -4,5 +4,4 @@ set -o errexit
 
 git clone --single-branch --branch main https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-build-scripts .scripts
 
-cp .init-scripts/install-dependencies.sh .scripts/
 .scripts/install-dependencies.sh
