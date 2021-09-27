bb7587dd9b7afb25d5589f4c0b5574d2c4b1a51a
Sonal Tandon <sonal.tandon@bbc.co.uk>
2021-09-27 13:47:19 +0100

    Adding and changing install-dependencies script

---
+++ .render/.init-scripts/install-dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o errexit
+
+[[ "${PWD##*/}" != ".render" ]] && echo Run this from the top-level directory && exit 1
+
+# shellcheck source=get-config.sh
+. .scripts/get-config.sh
+
+# All specs need Jekyll
+bundle install
--- .render/.init-scripts/make-build-tools.sh
+++ .render/.init-scripts/make-build-tools.sh
@@ -4,3 +4,4 @@ set -o errexit
 
 git clone --single-branch --branch main https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-build-scripts .scripts
 # .scripts/install-dependencies.sh
+cp install-dependencies.sh .scripts/
