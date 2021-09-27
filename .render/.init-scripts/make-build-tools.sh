9137175e12b953ca9324a83f83956ecb8bf7fbd2
Sonal Tandon <sonal.tandon@bbc.co.uk>
2021-09-27 13:49:09 +0100

    Adding line to run the install-dependencies script

--- .render/.init-scripts/make-build-tools.sh
+++ .render/.init-scripts/make-build-tools.sh
@@ -3,5 +3,6 @@
 set -o errexit
 
 git clone --single-branch --branch main https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-build-scripts .scripts
-# .scripts/install-dependencies.sh
+
 cp install-dependencies.sh .scripts/
+.scripts/install-dependencies.sh
